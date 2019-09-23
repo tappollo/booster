0bf58aecca3b79854491012b916a4845dafa170c
Kyle Fang <zhigang1992@gmail.com>
2019-09-23 16:15:23 +0800

    feat: upgrade to latest version of RN

--- app/src/functions/types.ts
+++ app/src/functions/types.ts
@@ -1,39 +1,5 @@
 import { AppDate } from "./envTypes";
 
-export interface UserInfo {
-  displayName?: string;
-  email?: string;
-  phoneNumber?: string;
-  photoURL?: string;
-  providerId: string;
-  uid: string;
-}
-
-export interface Conversation {
-  _duplicateLookup: string;
-  userIds: string[];
-  userDetails: {
-    [key: string]: UserInfo;
-  };
-  updatedAt: AppDate;
-  latestMessage?: Message;
-}
-
-export interface Message {
-  text: string;
-  createdBy: string;
-  createdAt?: AppDate;
-  image?: string;
-  system?: boolean;
-}
-
-export interface UserStatus {
-  online?: boolean;
-  disconnectedAt?: Date;
-  conversationId?: string;
-  isTyping?: boolean;
-}
-
 export interface Doc<T> {
   id: string;
   doc: T;
