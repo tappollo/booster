d78bdccb80dd7ecd41d5e60db483644081909b38
Kyle Fang <zhigang1992@gmail.com>
2019-09-24 18:24:59 +0800

    feat: use phone number input

--- app/src/pages/onboarding/ContinueWithPhonePage.tsx
+++ app/src/pages/onboarding/ContinueWithPhonePage.tsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { BigTitle } from "../../components/Title";
 import { BigButton } from "../../components/Button";
 import { Caption } from "react-native-paper";
 import { PageContainer } from "../../components/Page";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 import { Text } from "react-native";
+import PhoneNumberInputBox, {
+  defaultCountry
+} from "./components/PhoneNumberInput";
 
 const ContinueWithPhonePage: NavigationStackScreenComponent<{}> = ({
   navigation
 }) => {
+  const [country, setCountry] = useState(defaultCountry);
+  const [phone, setPhone] = useState("");
   return (
     <PageContainer>
       <BigTitle>Enter your{"\n"}mobile number</BigTitle>
+      <PhoneNumberInputBox
+        country={country}
+        phone={phone}
+        onPhoneChange={setPhone}
+      />
       <BigButton
         onPress={() => {
           navigation.push("VerifySMSCodePage");
---
+++ app/src/pages/onboarding/components/PhoneNumberInput.tsx
@@ -0,0 +1,109 @@
+import styled from "styled-components";
+import {
+  StyleProp,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ViewStyle,
+  View
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import React, { useState } from "react";
+import theme from "../../../styles/theme";
+
+export const defaultCountry: Country = {
+  name: "United States",
+  dial_code: "+1",
+  code: "US",
+  flag: "🇺🇸"
+};
+
+export interface Country {
+  name: string;
+  dial_code: string;
+  code: string;
+  flag: string;
+}
+
+const Container = styled(View)`
+  flex-direction: row;
+`;
+
+const CountryFlag = styled(Text)`
+  font-size: 27px;
+`;
+
+const Input = styled(TextInput).attrs({
+  maxLength: 16,
+  keyboardType: "phone-pad",
+  placeholder: "(123) 456-7890"
+})``;
+
+interface PhoneNumberInputBoxProps {
+  country: Country;
+  phone?: string;
+  onPhoneChange?: (phone: string) => void;
+  onSelectCountry?: () => void;
+  style?: StyleProp<ViewStyle>;
+}
+
+export const formatPhoneNumber = (text: string): string => {
+  const zip = text.substring(0, 3);
+  const middle = text.substring(3, 6);
+  const last = text.substring(6, 10);
+  if (text.length > 6) {
+    return `(${zip}) ${middle} - ${last}`;
+  } else if (text.length > 3) {
+    return `(${zip}) ${middle}`;
+  } else if (text.length > 0) {
+    return `(${zip}`;
+  } else {
+    return "";
+  }
+};
+
+const PhoneNumberInputBox = (props: PhoneNumberInputBoxProps) => {
+  const [focus, setFocus] = useState(false);
+  return (
+    <Container
+      style={{
+        borderBottomColor: focus
+          ? theme.colors.primary
+          : theme.colors.placeholder
+      }}
+    >
+      <TouchableOpacity
+        onPress={props.onSelectCountry}
+        style={{ flexDirection: "row", alignItems: "center" }}
+      >
+        <CountryFlag>{props.country.flag}</CountryFlag>
+        <Icon
+          name="md-arrow-dropdown"
+          color={theme.colors.placeholder}
+          size={18}
+        />
+        <Text style={{ marginHorizontal: 8, textAlignVertical: "bottom" }}>
+          {props.country.dial_code}
+        </Text>
+      </TouchableOpacity>
+      <Input
+        style={{ flex: 1 }}
+        value={
+          props.country.code === "US"
+            ? formatPhoneNumber(props.phone || "")
+            : props.phone || ""
+        }
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        onChangeText={value => {
+          const phone = value.replace(/\D/g, "");
+          if (props.onPhoneChange) {
+            props.onPhoneChange(phone);
+          }
+        }}
+      />
+    </Container>
+  );
+};
+
+export default PhoneNumberInputBox;
