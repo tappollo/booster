9ea930d0ce9c3a987d063d7f4b1a8c56e3ecda44
Kyle Fang <zhigang1992@gmail.com>
2020-01-03 18:08:51 +0800

    feat: move logout to user page

--- app/src/booster/pages/home/HomePage.tsx
+++ app/src/booster/pages/home/HomePage.tsx
@@ -1,20 +1,3 @@
-import { BigButton } from "../../components/Button";
-import auth from "@react-native-firebase/auth";
-import React, { useContext } from "react";
-import { AppRouteContext } from "../Routes";
-
-const HomePage = () => {
-  const { resetRoute } = useContext(AppRouteContext);
-  return (
-    <BigButton
-      onPress={async () => {
-        await auth().signOut();
-        resetRoute?.();
-      }}
-    >
-      Logout
-    </BigButton>
-  );
-};
+const HomePage = () => null;
 
 export default HomePage;
--- app/src/booster/pages/user/UserPage.tsx
+++ app/src/booster/pages/user/UserPage.tsx
@@ -1,3 +1,19 @@
-const UserPage = () => null;
+import styled from "styled-components/native";
+import React from "react";
+import LogoutButton from "./components/LogoutButton";
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  padding: 20px;
+`;
+
+const UserPage = () => {
+  return (
+    <Container>
+      <LogoutButton />
+    </Container>
+  );
+};
 
 export default UserPage;
