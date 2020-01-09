f4b9ec2b1f191a49a69da4e6a1291b981e2ce10e
Kyle Fang <zhigang1992@gmail.com>
2020-01-09 14:16:17 +0800

    wip - add chat

--- app/src/booster/functions/chat.ts
+++ app/src/booster/functions/chat.ts
@@ -2,6 +2,46 @@ import { useEffect } from "react";
 import database from "@react-native-firebase/database";
 import { currentUserId } from "./user";
 import { AppState, AppStateStatus } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+import { keyOf, useListenQuery } from "./firebase/firestore";
+import { Conversation, Profile } from "./types";
+
+export const useConversations = () => {
+  return useListenQuery<Conversation>(
+    firestore()
+      .collection("chats")
+      .where(keyOf<Conversation>("available"), "==", true)
+      .where(keyOf<Conversation>("userIds"), "array-contains", currentUserId())
+      .orderBy("updatedAt", "desc")
+  );
+};
+
+export const useNewContacts = () => {
+  const { value: contacts = [], loading: contactLoading } = useListenQuery<
+    Profile
+  >(firestore().collection("userProfiles"));
+  const {
+    value: conversations = [],
+    loading: conversationLoading
+  } = useConversations();
+  const userIdsInConversation: { [userId: string]: string } = {};
+  conversations.forEach(conv => {
+    const userId = conv.doc.userIds.find(id => id !== currentUserId());
+    if (userId) {
+      userIdsInConversation[userId] = conv.id;
+    }
+  });
+
+  return {
+    items: contacts
+      .filter(contact => contact.id !== currentUserId())
+      .map(contact => ({
+        ...contact,
+        conversationId: userIdsInConversation[contact.id]
+      })),
+    loading: contactLoading || conversationLoading
+  };
+};
 
 export const useUpdatePing = () => {
   useEffect(() => {
