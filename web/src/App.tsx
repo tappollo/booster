32cee1bee06cd3ae9818f3465b5988df0a4788d5
Kyle Fang <zhigang1992@gmail.com>
2020-01-16 11:54:28 +0800

    style: adjust screenshot size

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -15,7 +15,9 @@ const App: React.FC = () => {
       <div className="relative container mx-auto">
         <header className="flex flex-col md:flex-row md:h-screen md:pb-32 items-center justify-center">
           <div className="flex flex-col items-center mt-20 md:mt-0 md:items-start max-w-md md:ml-10">
-            <h1 className="text-5xl font-black md:text-6xl">Booster</h1>
+            <h1 className="text-5xl font-black md:text-6xl xl:text-7xl">
+              Booster
+            </h1>
             <p className="mt-2 text-sm px-12 text-center font-medium md:text-left md:pl-0 md:text-lg">
               Intergrated scaffolding codes,UI components, integration and
               delivery pipeline for fast pro-typing{" "}
@@ -32,15 +34,14 @@ const App: React.FC = () => {
           <img
             src={require("./assets/screenshot.png")}
             alt="Screenshot"
-            className="mt-12 object-contain md:mt-0"
-            style={{ height: 380 }}
+            className="mt-12 object-contain md:mt-0 screenshot"
           />
         </header>
-        <section className="mt-12 md:-mt-32">
-          <h2 className="font-bold text-3xl px-12 text-center">
+        <section className="mt-16 md:-mt-32">
+          <h2 className="font-bold text-3xl px-16 text-center">
             Your Digital Vault for Logins and More...
           </h2>
-          <ul className="mt-8 flex justify-around max-w-3xl mx-auto">
+          <ul className="mt-12 px-8 flex justify-around max-w-3xl mx-auto">
             <li>
               <Intro1 className="w-20 h-20" />
               <h4 className="mt-3 leading-tight text-sm font-medium text-center w-20">
