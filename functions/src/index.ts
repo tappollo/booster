aa5a0bfdf49fe45dcdded24bd75f33f7d93ad6a4
Kyle Fang <zhigang1992@gmail.com>
2020-02-04 22:01:28 +0800

    reorder

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,19 +4,17 @@ import * as functions from "firebase-functions";
 admin.initializeApp(functions.config().firebase);
 
 // Using lazy functions to improve cold boot speed
-
 const lazyFunctions = {
   status: () => require("./booster/status").statusCheck,
   image: () => require("./booster/image"),
-  // profile: () => require("./profile"),
   chat: () => require("./booster/chat")
+  // profile: () => require("./profile"),
   // user: () => require("./user"),
   // exp: () => require("./exports"),
   // search: () => require("./search")
 };
 
 const functionName = process.env.FUNCTION_NAME;
-
 (Object.keys(lazyFunctions) as Array<keyof typeof lazyFunctions>).forEach(
   name => {
     if (!functionName || functionName.startsWith(name)) {
