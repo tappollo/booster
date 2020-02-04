0956e8307ced5ad000aa1a555b8e750449acfbfb
Kyle Fang <zhigang1992@gmail.com>
2020-02-04 19:19:46 +0800

    fix: build

--- functions/src/booster/status.ts
+++ functions/src/booster/status.ts
@@ -3,7 +3,7 @@ import * as config from "../server.json";
 import * as version from "../version.json";
 
 export const statusCheck = functions.https.onRequest(async (req, resp) => {
-  console.log(`Status page requested, version ${version.current}`);
+  console.log(`Status page requested, version ${version.hash}`);
   resp.send({
     name: config.name,
     version
