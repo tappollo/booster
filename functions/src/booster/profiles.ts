ecfec6e72a65afac793cfc3778de2affa4c8b50a
Kyle Fang <zhigang1992@gmail.com>
2020-02-16 12:47:33 +0800

    feat: add mobx and readonly profile

--- functions/src/booster/profiles.ts
+++ functions/src/booster/profiles.ts
@@ -1,5 +1,5 @@
 import * as admin from "firebase-admin";
-import { PrivateProfile, Profile } from "../types";
+import { PrivateProfile, Profile, ReadonlyProfile } from "../types";
 
 export const getUserProfile = async (uid: string) => {
   const snapshot = await admin
@@ -23,6 +23,19 @@ export const getPrivateProfile = async (uid: string) => {
   const profile: PrivateProfile = snapshot.data() as any;
   if (!snapshot.exists || profile == null) {
     throw new Error(`User ${uid}'s private profile does not exist`);
+  }
+  return profile;
+};
+
+export const getReadonlyProfile = async (uid: string) => {
+  const snapshot = await admin
+    .firestore()
+    .collection("userReadonlyProfiles")
+    .doc(uid)
+    .get();
+  const profile: ReadonlyProfile = snapshot.data() as any;
+  if (!snapshot.exists || profile == null) {
+    throw new Error(`User ${uid}'s read only profile does not exist`);
   }
   return profile;
 };
