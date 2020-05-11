deda8c091d2fc227c37f97a4c068d2a5accbfc8f
Kyle Fang <zhigang1992@gmail.com>
2020-05-11 21:39:42 +0800

    feat: add script to deploy all

--- app/publishCodePush.ts
+++ app/publishCodePush.ts
@@ -4,37 +4,47 @@ import * as config from "./build.json";
 import { version } from "./package.json";
 import { execSync } from "child_process";
 
-const platform = process.argv[2]?.toLowerCase() as "ios" | "android";
+function deployPlatform(target: "ios" | "android") {
+  const { app_name, code_push_env, owner_name } = config.appcenter[target];
+
+  const hash = execSync("git rev-parse HEAD").toString().trim();
+  const count = execSync("git rev-list --count HEAD").toString().trim();
+
+  const release = `${version}-${count}-${hash.slice(0, 5)}`;
+
+  execSync("rm -rf ./build/CodePush");
+
+  execSync(
+    `appcenter codepush release-react --sourcemap-output --output-dir ./build -a ${owner_name}/${app_name} -k codepushsign.pem -d ${code_push_env} -t ${version} ${extraArgs}`,
+    { cwd: __dirname, stdio: "inherit" }
+  );
+
+  execSync(
+    `./node_modules/.bin/sentry-cli react-native appcenter ${owner_name}/${app_name} ${platform} ./build/CodePush --release-name '${release}' --deployment ${code_push_env}`,
+    {
+      cwd: __dirname,
+      stdio: "inherit",
+      env: {
+        ...process.env,
+        SENTRY_PROPERTIES: `./${platform}/sentry.properties`,
+      },
+    }
+  );
+}
+
+const platform = process.argv[2]?.toLowerCase() as "ios" | "android" | "all";
 
 const extraArgs = process.argv.slice(3).join(" ");
 
-if (!["ios", "android"].includes(platform)) {
-  console.log(`./publishCodePush.ts ios|android`);
+if (!["ios", "android", "all"].includes(platform)) {
+  console.log("./publishCodePush.ts ios|android|all");
   process.exit(1);
 }
 
-const { app_name, code_push_env, owner_name } = config.appcenter[platform];
+if (platform !== "android") {
+  deployPlatform("ios");
+}
 
-const hash = execSync("git rev-parse HEAD").toString().trim();
-const count = execSync("git rev-list --count HEAD").toString().trim();
-
-const release = `${version}-${count}-${hash.slice(0, 5)}`;
-
-execSync(`rm -rf ./build/CodePush`);
-
-execSync(
-  `appcenter codepush release-react --sourcemap-output --output-dir ./build -a ${owner_name}/${app_name} -k codepushsign.pem -d ${code_push_env} -t ${version} ${extraArgs}`,
-  { cwd: __dirname, stdio: "inherit" }
-);
-
-execSync(
-  `./node_modules/.bin/sentry-cli react-native appcenter ${owner_name}/${app_name} ${platform} ./build/CodePush --release-name '${release}' --deployment ${code_push_env}`,
-  {
-    cwd: __dirname,
-    stdio: "inherit",
-    env: {
-      ...process.env,
-      SENTRY_PROPERTIES: `./${platform}/sentry.properties`,
-    },
-  }
-);
+if (platform !== "ios") {
+  deployPlatform("android");
+}
