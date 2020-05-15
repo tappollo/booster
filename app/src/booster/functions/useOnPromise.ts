288b82e445e952fd79c99417641a446cfc84eb00
Kyle Fang <zhigang1992@gmail.com>
2020-05-15 18:34:23 +0800

    fix: use generic

--- app/src/booster/functions/useOnPromise.ts
+++ app/src/booster/functions/useOnPromise.ts
@@ -1,12 +1,12 @@
 import { Alert } from "react-native";
 import { useCallback, useState } from "react";
 
-export function useOnPromise(action: () => Promise<void>) {
+export function useOnPromise<T>(action: () => Promise<T>) {
   const [loading, setLoading] = useState(false);
   const onPromise = useCallback(async () => {
     try {
       setLoading(true);
-      await action();
+      return await action();
     } catch (e) {
       Alert.alert(e.message);
     } finally {
